7e9f3314220edb5d3cc775d694ca3768f63f8f8e
aripramuja <105218009@student.universitaspertamina.ac.id>
2022-01-15 15:16:25 +0700

    handling btn

--- app/src/main/java/com/digeometri/digeometri/LoginActivity.kt
+++ app/src/main/java/com/digeometri/digeometri/LoginActivity.kt
@@ -3,19 +3,27 @@ package com.digeometri.digeometri
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+//import android.view.View
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var RegisterBtn : Button
+    lateinit var GoToMainBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        btnRegLogin.setOnClickListener {
+
+        RegisterBtn = findViewById(R.id.btnRegLogin)
+        GoToMainBtn = findViewById(R.id.btnMainPage)
+
+        RegisterBtn.setOnClickListener {
             startActivity(Intent(this,RegisterActivity::class.java))
             overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
         }
-        setContentView(R.layout.activity_login)
-        btnmainPage.setOnClickListener {
+
+        GoToMainBtn.setOnClickListener {
             startActivity(Intent(this,MainActivity::class.java))
             overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
         }
