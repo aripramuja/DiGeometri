4e5fd7e7d8932a2a005102f6d8f5f7226129c222
aripramuja <105218009@student.universitaspertamina.ac.id>
2022-01-15 16:10:36 +0700

    handling button reg

--- app/src/main/java/com/digeometri/digeometri/RegisterActivity.kt
+++ app/src/main/java/com/digeometri/digeometri/RegisterActivity.kt
@@ -1,16 +1,28 @@
 package com.digeometri.digeometri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var LoginBtn : Button
+    lateinit var RegisterBtn : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_register)
 
-        btnLogRegister.setOnClickListener {
+        LoginBtn = findViewById(R.id.btnLogRegister)
+        RegisterBtn =findViewById(R.id.btnRegister)
+
+        RegisterBtn.setOnClickListener {
+            startActivity(Intent(this,LoginActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+        LoginBtn.setOnClickListener {
             onBackPressed()
         }
     }
