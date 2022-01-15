83386528972831e9ce38129f1445d776c019acbb
aripramuja <105218009@student.universitaspertamina.ac.id>
2022-01-15 13:41:22 +0700

    ui register & ui login

---
+++ app/src/main/java/com/digeometri/digeometri/LoginActivity.kt
@@ -0,0 +1,19 @@
+package com.digeometri.digeometri
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        btnRegLogin.setOnClickListener {
+            startActivity(Intent(this,RegisterActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+    }
+}
+
