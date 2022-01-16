abed04d93cdfe15c6cf459a44ca3d416a2e49386
aripramuja <105218009@student.universitaspertamina.ac.id>
2022-01-16 10:07:36 +0700

    main page geometri

---
+++ app/src/main/java/com/digeometri/digeometri/GeometryMainActivity.kt
@@ -0,0 +1,12 @@
+package com.digeometri.digeometri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeometryMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geometry_main)
+    }
+}
--- app/src/main/java/com/digeometri/digeometri/GeometryTypesActivity.kt
+++ app/src/main/java/com/digeometri/digeometri/GeometryTypesActivity.kt
@@ -1,12 +1,21 @@
 package com.digeometri.digeometri
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class GeometryTypesActivity : AppCompatActivity() {
+    lateinit var FlatMainBtn : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_geometry_types)
+
+        FlatMainBtn = findViewById(R.id.btnFlatMain)
+        FlatMainBtn.setOnClickListener {
+            startActivity(Intent(this,GeometryMainActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
     }
 }
