24aa5d5fabd6c3b90abf253e70a2d1fd4455c178
mfaridft99 <faridurangsunda@gmail.com>
2022-01-23 22:58:23 +0700

    update

--- app/src/main/java/com/digeometri/digeometri/GeometryTypesActivity.kt
+++ app/src/main/java/com/digeometri/digeometri/GeometryTypesActivity.kt
@@ -6,14 +6,64 @@ import android.os.Bundle
 import android.widget.ImageView
 
 class GeometryTypesActivity : AppCompatActivity() {
-    lateinit var FlatMainBtn : ImageView
+    lateinit var flatSgtBtn : ImageView
+    lateinit var flatPsgBtn : ImageView
+    lateinit var flatPPBtn : ImageView
+    lateinit var flatLkgBtn : ImageView
+    lateinit var flatJGBtn : ImageView
+    lateinit var flatLLBtn : ImageView
+    lateinit var flatTrapBtn : ImageView
+    lateinit var flatBKBtn : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_geometry_types)
 
-        FlatMainBtn = findViewById(R.id.btnFlatMain)
-        FlatMainBtn.setOnClickListener {
+        flatSgtBtn = findViewById(R.id.btnFlatSegitiga)
+        flatPsgBtn = findViewById(R.id.btnFlatPersegi)
+        flatPPBtn = findViewById(R.id.btnFlatPerPanjang)
+        flatLkgBtn = findViewById(R.id.btnFlatLingkaran)
+        flatJGBtn = findViewById(R.id.btnFlatJarGen)
+        flatLLBtn = findViewById(R.id.btnFlatLayang2)
+        flatTrapBtn = findViewById(R.id.btnFlatTrapesium)
+        flatBKBtn = findViewById(R.id.btnFlatBlhKetupat)
+
+        flatSgtBtn.setOnClickListener {
+            startActivity(Intent(this,GeometryMainActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+
+        flatPsgBtn.setOnClickListener {
+            startActivity(Intent(this,PersegiMainActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+
+        flatPPBtn.setOnClickListener {
+            startActivity(Intent(this,PPanjangMainActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+
+        flatLkgBtn.setOnClickListener {
+            startActivity(Intent(this,LingkaranMainActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+
+        flatJGBtn.setOnClickListener {
+            startActivity(Intent(this,GeometryMainActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+
+        flatLLBtn.setOnClickListener {
+            startActivity(Intent(this,GeometryMainActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+
+        flatTrapBtn.setOnClickListener {
+            startActivity(Intent(this,GeometryMainActivity::class.java))
+            overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
+        }
+
+        flatBKBtn.setOnClickListener {
             startActivity(Intent(this,GeometryMainActivity::class.java))
             overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
         }
